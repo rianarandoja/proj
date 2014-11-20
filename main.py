c768035c127331476da6844ca8e8255596792360
Kaspar Papli <kaspar.papli@gmail.com>
2014-11-21 00:43:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,32 @@ def getFunctionWithComs(user_coms):
     user_coms_str = ''.join(user_coms)
     for func in Function_keywords.func_kw_list:
         for i in range(1, len(func)):
-            if func[i] in user_coms_str.lower().strip():
+            if func[i] in user_coms_str.lower():
                 return func[0]
     return None
+
+def restoreEntry():
+    main_ent_st.configure("EntryStyle.TEntry", fieldbackground="white", foreground="black")
+    main_ent.delete(0, END)
+    main_ent.insert(0, last_inp)
+
+def errorEntry():
+    main_ent_st.configure("EntryStyle.TEntry", fieldbackground="fireBrick3", foreground="#FFF5E7")
+    main_ent.delete(0, END)
+    main_ent.insert(0, 'Vigane sisend!')
+
+def incorrectInput():
+    global last_inp
+    last_inp = main_ent.get()
+    for i in range(0, 3, 2):
+        eval('root.after(' + str(400*i) + ', errorEntry)')
+        eval('root.after(' + str(400*(i+1)) + ', restoreEntry)')
 
 def main(*args):
     user_args_coms = Parse_input.parseInput(main_ent.get(), '[', ']')
     if user_args_coms == -1:
         # If user input is incorrect
-        info_lbl_var.set('Ebakorrektne sisend!')
-        root.after(2500, lambda: info_lbl_var.set(''))
+        incorrectInput()
     elif user_args_coms:
         # If user input is non-empty
         user_args, user_coms = user_args_coms
@@ -49,25 +65,36 @@ root = Tk()
 root.title("ReisidAafrikasse")
 root.wm_attributes("-topmost", 1)  # Always of top
 root.resizable(width=FALSE, height=FALSE)
+root.geometry('345x105')
 
 mainframe_st = ttk.Style()
-mainframe_st.configure('mainframe.TFrame', background='black')
-info_lbl_st = ttk.Style()
-info_lbl_st.configure('info_lbl.TLabel', background='black')
+mainframe_st.configure('mainframe.TFrame', background='white')
 
-mainframe = ttk.Frame(root, padding='20', style='mainframe.TFrame')
-mainframe.grid(row=0, column=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
+mainframe = ttk.Frame(root, style='mainframe.TFrame')
+mainframe.place(x=0, y=0, relwidth=1, relheight=1)
 
-main_ent = ttk.Entry(mainframe, width=50)
-main_ent.grid(row=2, column=1, sticky=(W, E))
+bg_img = PhotoImage(file='C.png')
+bg_lbl = Label(mainframe, image=bg_img)
+bg_lbl.place(x=0, y=0, relwidth=1, relheight=1)
+
+main_ent_st = ttk.Style()
+# Fimagzen
+# http://stackoverflow.com/questions/17635905/ttk-entry-background-colour [20.11.14]
+main_ent_st.element_create("plain.field", "from", "clam")
+main_ent_st.layout("EntryStyle.TEntry",
+                   [('Entry.plain.field', {'children': [(
+                       'Entry.background', {'children': [(
+                           'Entry.padding', {'children': [(
+                               'Entry.textarea', {'sticky': 'nswe'})],
+                      'sticky': 'nswe'})], 'sticky': 'nswe'})],
+                      'border':'2', 'sticky': 'nswe'})])
+
+
+main_ent = ttk.Entry(mainframe, width=50, style='EntryStyle.TEntry')
+main_ent.place(x=20, y=45)
 main_ent.focus_set()
 
-info_lbl_var = StringVar()
-ttk.Label(mainframe, textvariable=info_lbl_var, foreground='red', style='info_lbl.TLabel').grid(row=1, column=1, sticky=W)
-
-ttk.Button(mainframe, text='√Reisile!', command=main).grid(row=3, column=1, sticky=N)
+ttk.Button(mainframe, text='√Reisile!', command=main).place(x=135, y=65)
 main_ent.bind('<Return>', main)
 
 root.mainloop()
