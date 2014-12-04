1a49ec126452795efbde3ba2019e5c146228897b
Kaspar Papli <kaspar.papli@gmail.com>
2014-12-04 15:06:56 +0200

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,13 +1,18 @@
 import sympy
-x = sympy.symbols("x")
+#x = sympy.symbols("x") # Milleks seda vaja?
 
-def Calculator(expr, round_to="3"):
+
+def calculator(raw_expr, round_to="3"):
+    raw_expr = ''.join(raw_expr).strip()
     round_to = sympy.sympify(round_to)
-    expr = sympy.sympify(expr)
+    expr = sympy.sympify(raw_expr)
     answer = str(expr.evalf(round_to))
+    if raw_expr == answer:
+        raise SyntaxError
+    print(answer.rstrip(('.')))  # DB
     return answer.rstrip('.')
 
 if __name__ == '__main__':
-    print(Calculator("log(2)**2 - 6*x/sqrt(17)", "7"))
-    print(Calculator("log(3)", '6'))
-    print(Calculator("6*17"))
+    print(calculator("log(2)**2 - 6*x/sqrt(17)", "7"))
+    print(calculator("log(3)", '6'))
+    print(calculator("ilm T7*y - 30"))
