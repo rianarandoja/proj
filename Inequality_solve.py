ef73c0edf7dfe7a3af8a2de115b525268e46dc98
rianarandoja <rianarandoja@gmail.com>
2014-12-06 10:29:52 +0200

    Update Inequality_solve.py

--- Inequality_solve.py
+++ Inequality_solve.py
@@ -1,22 +1,45 @@
 from Equation_manipulation import *
-from sympy import solve
+from sympy import solve, symbols
+from tkinter import *
 x = symbols("x")
 
+def InEqResultWindow(result, user_input="user input"):
+    root = Tk()
+    root.title("Aafrika")
+
+    y_scrollbar = Scrollbar(root)
+    y_scrollbar.pack(side=RIGHT, fill=Y)
+
+    listbox = Listbox(root, yscrollcommand=y_scrollbar.set, selectmode=MULTIPLE, height=6, width=50,
+                      selectbackground="gold2", activestyle="none")
+    listbox.pack(side=LEFT, fill=BOTH, expand=1)
+
+    y_scrollbar.config(command=listbox.yview)
+
+    listbox.insert(0, "")
+    listbox.insert(1, " "*30 + user_input)
+    listbox.insert(2, "")
+
+    for item in result:
+        if item != "":
+            listbox.insert(END, str("           •   " + item.strip()))
+
+    root.mainloop()
 
 def solveInequality(inequality):
-    # solves inequalities, takes argument as string
-    result = str(solve(getMissingMultiplic(inequality)))
+    inequality = "".join(inequality)
+    inequality = optimizeEquationForSympy(inequality)
+    result = str(solve(inequality))
+    result = optimizeEquationForSympy(result)
     if result == 'False':
-        result = "Lahendid puuduvad"
+        result = ["Lahendid puuduvad"]
     else:
         im_index = result.index("im")
-        variable = result[im_index + 3]
+        variable = result[im_index + 4]
         result = result.replace(variable, "x")
-        if result == "im(x) == 0":
-            result = "Lahendiks sobivad kõik reaalarvud"
+        if result == "im(x) == 0" or result == "im*(x)-=0":
+            result = ["Lahendiks sobivad kõik reaalarvud"]
         else:
-            result = result[4:-1]
-            # removes the first "and" that comes with imaginary result
             # removes imaginary results
             separate_results = result.split(",")
             to_remove = []
@@ -28,23 +51,25 @@ def solveInequality(inequality):
             for el in to_remove:
                 separate_results.remove(el)
             result = ",".join(separate_results)
-            result = (result.replace('re(x), re(x)', 'x')
+            result = (result.replace('re(x),re(x)', ' x ')
                             .replace(',', ' või')
                             .replace('And', '')
-                            .replace('re(x)', 'x')
+                            .replace('re(x)', ' x ')
                             .replace("Or", ""))
-            if "sqrt" not in result:
+            if "**0.5" not in result:
                 result = (result.replace('(', '')
                                 .replace(')', ''))
-            else:
-                if result[-1] == ")" and result[-2] == ")":
-                    # deletes the unnecessary bracket from ending
-                    result = result[:-1]
-                if result[0] == "(":
-                    result = result[1:]
-                    # deletes the first unnecessary bracket
-        result = result.replace("x", variable).strip()
+            result = result.replace("x", variable).strip().replace("*", "")
+            result = result.replace("(", "").replace(")", "")
+            result = result.replace("<", " < ").replace(">", " > ")
+            result = result.split(" või ")
     return result
 
+def Inequality(user_input):
+    user_input = "".join(user_input)
+    result = solveInequality(user_input)
+    InEqResultWindow(result, user_input)
+
+
 if __name__ == '__main__':
-    print(solveInequality('4x-4 < 0'))
+    print(Inequality(["(x-4)(5+x)(x-2)<0"]))
